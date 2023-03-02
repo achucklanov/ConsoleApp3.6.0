4af7a4701b4bb8574a84dc8fa86198a7cb56f99d
AndreyChuck <a.chucklanov@yandex.ru>
2023-03-02 21:10:52 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp3.6.0/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp3._6._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // вариант1 
+            Console.WriteLine("Вариант 1");
+            Console.WriteLine();
+            double A = 5;
+            double B = 2;
+            double R = A / B;
+            Console.WriteLine("  5/2 = {0}", R);
+            Console.WriteLine();
+            Console.WriteLine();
+
+            // вариант 2
+            Console.WriteLine("Вариант 2");
+            Console.WriteLine();
+            double Res = 5.0 / 2.0;
+            Console.WriteLine("  5/2 = {0}", Res);
+        }
+    }
+}
